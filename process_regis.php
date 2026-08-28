c4669efb28d88a981c96cfa0babe8b2a132fd462
ChuDuc00 <thuduc19102006@gmail.com>
2026-08-28 20:51:48 +0700

    backend manage_regis

--- process_regis.php
+++ process_regis.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// 1. Kết nối CSDL quanly_sukien
 $conn = mysqli_connect("localhost", "root", "", "quanly_sukien");
 
 if (!$conn) {
