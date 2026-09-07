950d84423cae4703612f2260dcf988cff3ed27b8
MaiPhuong <vumaiphuong2006t@gmail.com>
2026-09-07 07:48:52 +0700

    'xoa'

--- includes/sidebar-organizer.php
+++ includes/sidebar-organizer.php
@@ -2,22 +2,10 @@
     <span class="sidebar-text">Trang chủ</span>
 </a>
 
-<a href="my-club.php" class="sidebar-item<?= sidebar_active('my-club.php') ?>">
-    <span class="sidebar-text">Câu lạc bộ của tôi</span>
-</a>
-
-<a href="club-members.php" class="sidebar-item<?= sidebar_active('club-members.php') ?>">
-    <span class="sidebar-text">Thành viên CLB</span>
-</a>
-
 <a href="events.php" class="sidebar-item<?= sidebar_active('events.php') ?>">
     <span class="sidebar-text">Sự kiện</span>
 </a>
 
 <a href="event_registrations.php" class="sidebar-item<?= sidebar_active('event_registrations.php') ?>">
     <span class="sidebar-text">Phê duyệt đăng ký sự kiện</span>
-</a>
-
-<a href="announcements.php" class="sidebar-item<?= sidebar_active('announcements.php') ?>">
-    <span class="sidebar-text">Thông báo</span>
 </a>
